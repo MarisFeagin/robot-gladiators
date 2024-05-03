229c1c79df229ed9331f9adfcde101ebcac467de
Maris Feagin <mfeagin64@gmail.com>
2024-05-03 16:17:17 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,17 +13,21 @@ var fight = function(enemyNames) {
   while(playerHealth > 0 && enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
     if (promptFight === "fight" || promptFight === "FIGHT") {
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + ". "
       );
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + ". "
       );
 
       if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert(enemyNames + " has died!");
         break;
       }
 
@@ -38,7 +42,7 @@ var fight = function(enemyNames) {
 
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log ("playerMoney", playerMoney);
         break;
     }
@@ -59,7 +63,7 @@ var startGame = function() {
     if (playerHealth < 0){
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     debugger;
     fight(pickedEnemyName);
     if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -139,5 +143,11 @@ var endGame = function() {
 
 
 }
+
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * (21)) + 40;
+  
+  return value;
+};
 
 fight();
